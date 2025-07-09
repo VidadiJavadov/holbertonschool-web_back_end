6808eb4a98b59698231611275c493269a3f60d81
VidadiJavadov <vidadi4000@gmail.com>
2025-07-09 15:14:50 +0400

    Update 10-update_topics.py

--- NoSQL/10-update_topics.py
+++ NoSQL/10-update_topics.py
@@ -3,4 +3,4 @@
 
 def update_topics(mongo_collection, name, topics):
     """Updating topics"""
-    mongo_collection.update({"name": name}, {"$set": {"topics": topics}})
+    mongo_collection.update_many({"name": name}, {"$set": {"topics": topics}})
